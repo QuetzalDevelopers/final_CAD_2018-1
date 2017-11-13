6c30eb2c281d64211acbbcfd46328ed64a43dd1d
sofia delgado gonzalez <soff.dg18@gmail.com>
2017-11-12 21:59:04 -0600

    funcion que lee y divide en intervalos de n

--- cifrado_de_hill.c
+++ cifrado_de_hill.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void lecturaTeclado(int n, int *mat); 
-void lecturaDeArchivoTXT(FILE * fichero, char * cad, int tam);
+int lecturaTeclado(int n, int *mat); 
+void lecturaDeArchivoTXT(FILE * fichero, char * cad, int n);
 
 int main(int argc, char const *argv[])
 {
@@ -11,13 +11,13 @@ int main(int argc, char const *argv[])
 	int tam,*mat,n;
 	FILE* fichero; 
 
-	lecturaTeclado(n,mat);	
-	lecturaDeArchivoTXT(fichero, cad, tam);
+	n=lecturaTeclado(n,mat);	
+	lecturaDeArchivoTXT(fichero, cad, n);
 
 	return 0;
 
 }
-void lecturaTeclado(int n, int * mat){
+int lecturaTeclado(int n, int * mat){
 	//funcion que permite la lectura de datos de teclado de n y de la matriz
 	int i,j,val; 
 	printf("Ingrese el valor de n\n");
@@ -30,23 +30,50 @@ void lecturaTeclado(int n, int * mat){
 			scanf("%d",&val);
 			mat[i]=val;
 	}
+	return n;
 }
-void lecturaDeArchivoTXT(FILE * fichero, char * cad, int tam){
-	int letra, contador,i;
+
+void lecturaDeArchivoTXT(FILE * fichero, char * cad, int n){
+	int letra, contador,i,j,inicio,fin,modulo;
 	fichero = fopen("ejemplo.txt","r");
 	contador=0; 
 	while(letra!=EOF)//para saber el tamaño de la cadena
     {
         letra= getc(fichero);  //obtienes un caracter y lo guardas, el valor es su equivalente en ascii, por eso es de tipo int
         contador++;
-        printf("%c\n",letra);
     }
     fclose(fichero);
-    printf("\n contador %d\n",contador);
+    contador=contador-1;
+    modulo= contador%n;//revisar si se pueden agrupar en grupos de n
+   	printf("modulo=> %d mod %d = %d\n",contador,n, modulo);
+   	if(modulo>0){
+    		contador=contador+(n-modulo); 
+    	}
+    
     cad  = (char *)malloc(contador * sizeof(char));
     fichero = fopen("ejemplo.txt","r");
-    for(i=0; i<contador; i++){cad[i]=getc(fichero);printf("%c",cad[i]);}
+    for(i=0; i<contador; i++){
+    	cad[i]=getc(fichero);
+    	if(i>(contador-n)){
+    		cad[i]='x';
+    	}
+    }
 	fclose(fichero);
+	inicio=0;
+	fin=n;
+	printf("\n");
+    
+    //para imprimir en periodos de n 
+	for(i=0; i<(contador/n); i++){
+		for (j =inicio; j < fin; ++j){
+			printf("%c",cad[j]);	
+		}
+		printf("\n");
+		inicio=inicio+n;
+		fin=inicio+n; 
+	}
+		
 }
+
 
 
